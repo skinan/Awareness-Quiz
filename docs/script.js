09681f0ce44b364e8b83ca5ae7a26f0308fc7b0f
Sakib Khan Inan <sakib.khaninan@gmail.com>
2020-03-27 00:04:51 +0600

    quiz final update

--- docs/script.js
+++ docs/script.js
@@ -28,19 +28,19 @@ var allQuestions = [
     }
     , {
         question: "What is forbidden to prevent the virus from spreading?",
-        choices: ["Sleeping", "Eating Foods", "Shaking Hands & Hugging", "Wearing masks"],
+        choices: ["Sleeping", "Eating Foods", "Shaking Hands & engaging in physical contact", "Wearing masks"],
         answer: 2
     }
     , {
         question: "What should you do now to prevent the virus from spreading in  your community?",
-        choices: ["Stay at home & keep distance from eachother", "Roaming Around Unnecessarily",
+        choices: ["Stay at home & keep physical distance from each other", "Roaming Around Unnecessarily",
             "Visit Relatives Houses",
             "Party With Friends"],
         answer: 0
     }
     , {
         question: "Don't touch you face, nose, eyes without _________ first",
-        choices: ["Dirty Hands", "Cleaning Hands", "Having Food", "Going Outside"],
+        choices: ["Sleeping", "Cleaning Hands", "Having Food", "Going Outside"],
         answer: 1
     },
     {
@@ -51,7 +51,7 @@ var allQuestions = [
         answer: 3
     },
     {
-        question: "What is the minimum period of quarantine for COVID-19?",
+        question: "What is the minimum length of self-quarantine for COVID-19?",
         choices: ["1 Day", "10 Hours", "14 Days",
             "5 Minutes"],
 
@@ -59,7 +59,7 @@ var allQuestions = [
     },
     {
         question: "What is meant by Social Distancing?",
-        choices: ["Maintaining physical distancing from everyone", "Blocking people at social websites",
+        choices: ["Maintaining proper physical distance from everyone", "Blocking people at social websites",
             "Family get together",
             "Vacation trip with friends"],
 
@@ -75,18 +75,52 @@ var allQuestions = [
     },
     {
         question: "What should you do if you notice the symptoms of COVID-19 in yourself?",
-        choices: ["Going outside to have a walk", "Start doing panic", "Stay away from others and seek proper medical help",
+        choices: ["Going outside to have a walk", "Start doing panic", "Keep distance from others and seek proper medical help",
             "Spend quality time with family members"],
 
         answer: 2
     },
     {
-        question: "How can you contribute in the fight against virus?",
-        choices: ["Not following instructions","Going outside & creating crowd", "Following the instructions of government properly",
+        question: "How can you contribute in the fight against the virus?",
+        choices: ["Not following instructions","Going outside & creating crowd", "Staying at home & " +
+        "following the instructions of government properly",
             "Believing in misconceptions about the virus"],
 
         answer: 2
+    },
+    {
+        question: "What you should not do?",
+        choices: ["Washing hands frequently with soap & water or hand sanitizer", "Wearing masks outside home", "Use a tissue or cover face " +
+        "with elbow while sneezing and coughing",
+            "Touching face or things with unwashed hands"],
+
+        answer: 3
+    },
+    {
+        question: "How to confirm the presence of COVID-19 in anyone's body?",
+        choices: ["Proper medical test by verified authority", "Asking your best friend", "Asking your neighbour",
+            "Discussion in social media"],
+
+        answer: 0
+    },
+    {
+        question: "The virus effected person should be kept __________",
+        choices: ["In isolation & proper medical treatment", "With healthy people", "With other effected people",
+            "With famliy & friends"],
+
+        answer: 0
+    },
+    {
+        question: "What you should not do?",
+        choices: ["Cleaning & disinfecting frequently touched objects", "Boiling your food properly while cooking",
+            "Following proper health instructions given by verified health organisations",
+            "Not maintaining cleanliness and hygiene "],
+
+        answer: 0
+
     }
+
+
 
 ];
 
@@ -115,12 +149,12 @@ function end() {
     $("h3").text("Share it with your friends. Raise awareness!");
     $("ul").hide();
     $("h2").text("You scored " + totalCorrect + " out of " + allQuestions.length + "." + " You are " +
-        ((totalCorrect / allQuestions.length) * 100) + "% aware!");
+        Math.floor(((totalCorrect / allQuestions.length) * 100)) + "% aware!");
     $("#try-again-container").show();
     $('#share-facebook').attr('href', "https://www.facebook.com/dialog/feed?app_id=532398964372093&display=popup&link=" +
         "https://skinan.github.io/Awareness-Quiz/&quote=I scored " +
         totalCorrect + " out of " + allQuestions.length + ". I am "
-        + ((totalCorrect / allQuestions.length) * 100) + "%" + " aware! It's your turn now!" + "&redirect_uri=https://facebook.com");
+        + Math.floor(((totalCorrect / allQuestions.length) * 100)) + "%" + " aware! It's your turn now!" + "&redirect_uri=https://facebook.com");
     //jQuery("meta[property='og\\:url']").attr("content", url);
     //$('#mdescription').attr('content', 'my new meta description');
     //$('title').html('my new meta title');
