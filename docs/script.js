ab3041ec03ac0d2c5c57c44350b49bc4556310c4
Sakib Khan Inan <sakib.khaninan@gmail.com>
2020-03-26 19:17:26 +0600

    Update

--- docs/script.js
+++ docs/script.js
@@ -71,8 +71,10 @@ function end() {
     $("h2").text("You scored " + totalCorrect + " out of " + allQuestions.length + "." + "You are " +
         ((totalCorrect / allQuestions.length) * 100) + "% aware!");
     $("#try-again-container").show();
-    //$('meta[property ="og:title"]').attr('content',"I scored " + ((totalCorrect / allQuestions.length) * 100) + "% on COVID-19 Awareness Quiz. " +
-    //"It's your turn now!");
+    $('#share-facebook').attr('href', "https://www.facebook.com/dialog/feed?app_id=532398964372093&display=popup&link=" +
+        "https://skinan.github.io/Awareness-Quiz/&quote=I scored " +
+        totalCorrect + " out of " + allQuestions.length + ". I am "
+        + ((totalCorrect/allQuestions.length)*100) + "%" + " aware! It's your turn now!" +"&redirect_uri=https://facebook.com");
     //jQuery("meta[property='og\\:url']").attr("content", url);
     //$('#mdescription').attr('content', 'my new meta description');
     //$('title').html('my new meta title');
