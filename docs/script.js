1917533f4a4c5aa8a806076e2def4956c1429946
Sakib Khan Inan <sakib.khaninan@gmail.com>
2020-03-26 18:48:50 +0600

    Title Update

--- docs/script.js
+++ docs/script.js
@@ -71,8 +71,8 @@ function end() {
     $("h2").text("You scored " + totalCorrect + " out of " + allQuestions.length + "." + "You are " +
         ((totalCorrect / allQuestions.length) * 100) + "% aware!");
     $("#try-again-container").show();
-    $('meta[property ="og:title"]').attr('content',"I scored " + ((totalCorrect / allQuestions.length) * 100) + "% on COVID-19 Awareness Quiz. " +
-    "It's your turn now!");
+    //$('meta[property ="og:title"]').attr('content',"I scored " + ((totalCorrect / allQuestions.length) * 100) + "% on COVID-19 Awareness Quiz. " +
+    //"It's your turn now!");
     //jQuery("meta[property='og\\:url']").attr("content", url);
     //$('#mdescription').attr('content', 'my new meta description');
     //$('title').html('my new meta title');
