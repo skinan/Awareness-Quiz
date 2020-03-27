be8767544887c4f983c235ae1fa5f8e4d110ab01
Sakib Khan Inan <sakib.khaninan@gmail.com>
2020-03-27 14:57:02 +0600

    unnecessary cut

--- docs/script.js
+++ docs/script.js
@@ -155,9 +155,7 @@ function end() {
         "https://skinan.github.io/Awareness-Quiz/&quote=I scored " +
         totalCorrect + " out of " + allQuestions.length + ". I am "
         + Math.floor(((totalCorrect / allQuestions.length) * 100)) + "%" + " aware! It's your turn now!" + "&redirect_uri=https://facebook.com");
-    //jQuery("meta[property='og\\:url']").attr("content", url);
-    //$('#mdescription').attr('content', 'my new meta description');
-    //$('title').html('my new meta title');
+
     restart();
 }
 
